52ff6c16ac27966e602a4d7d0f010d579ee58581
A-Cdeveloper <aleksandar@e-vlasotince.info>
2023-03-21 14:11:58 +0100

    connect Games component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import { useState } from "react";
 import { matches } from "./data/data";
 
-import { formatResult } from "./utils/arrayFn";
+import Games from "./components/Games";
 
 function App() {
   const [liveGames, setLiveGames] = useState(matches);
   const [finishGames, setFinishGames] = useState([]);
   return (
     <div className="board">
-      {/* live games */}
-      <div className="results">
-        {liveGames.map((game) => {
-          return (
-            <div className="singleMatch">
-              <div className="teams">
-                {game.teamHome} - {game.teamAway}
-              </div>
-
-              <div className="result">
-                {game.finalResult.length !== 0 &&
-                  formatResult(game.finalResult)}
-              </div>
-
-              <div className="action">
-                <button onClick={() => {}}>START</button>
-                <button onClick={() => {}}>UPDATE</button>
-                <button onClick={() => {}}>FINISH</button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-
-      {/* finished games */}
-
-      <div className="results">
-        {finishGames.map((game) => {
-          return (
-            <div className="singleMatch">
-              <div className="teams">
-                {game.teamHome} - {game.teamAway}
-              </div>
-
-              <div className="result">
-                {game.finalResult.length !== 0 &&
-                  formatResult(game.finalResult)}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      {liveGames.length !== 0 && <Games games={liveGames} finish={false} />}
+      {finishGames.length !== 0 && <Games games={finishGames} finish={true} />}
     </div>
   );
 }
